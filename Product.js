a298499dd8506755b42b5fe9e446a2833bafb6d3
Will Parks <wparks08@gmail.com>
2019-12-09 00:45:45 -0800

    remove print method from Product

--- Product.js
+++ Product.js
@@ -6,10 +6,10 @@ function Product(values) {
     this.stock_quantity = values.stock_quantity;
 }
 
-Product.prototype.print = function () {
-    console.log(
-        `ID: ${this.item_id} -- ${this.product_name}, ${this.department_name}`
-    ); //Print everything here
-}
+//fn: reduce quantity
+
+//fn: get price
+
+//fn: save?
 
 module.exports = Product;
