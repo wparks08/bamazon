071a6ca0e793a798dfa802d5c37ea790c36e81c5
Will Parks <wparks08@gmail.com>
2019-12-10 00:53:56 -0800

    add manager menu prompt

---
+++ bamazonManager.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const connection = require("./db");
+const inquirer = require("inquirer");
+var Table = require("cli-table");
+
+const Product = require("./Product");
+
+function promptManagerMenu() {
+    inquirer.prompt([
+        {
+            type: "list",
+            choices: [
+                { name: "View Products for Sale", value: "forSale", short: "View Products for Sale"},
+                { name: "View Low Inventory", value: "lowInventory", short: "View Low Inventory"},
+                { name: "Add to Inventory", value: "addInventory", short: "Add to Inventory"},
+                { name: "Add New Product", value: "newProduct", short: "Add New Product"}
+            ],
+            name: "selection"
+        }
+    ]).then(menu => {
+        switch (menu.selection) {
+            case "forSale":
+                console.log("forSale");
+                break;
+            case "lowInventory":
+                console.log("lowInventory");
+                break;
+            case "addInventory":
+                console.log("addInventory");
+                break;
+            case "newProduct":
+                console.log("newProduct");
+                break;
+        }
+    })
+}
+
+promptManagerMenu();
