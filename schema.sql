63ac7895153a043fef5b9463fe7b50e7794b9961
Will Parks <wparks08@gmail.com>
2019-12-13 17:52:02 -0800

    add department to schema

--- schema.sql
+++ schema.sql
@@ -6,8 +6,17 @@ USE bamazon;
 CREATE TABLE product(
     item_id INT AUTO_INCREMENT NOT NULL,
     product_name VARCHAR(255),
-    department_name VARCHAR(63),
+    department_id INT,
     price INT DEFAULT 0,
     stock_quantity INT,
-    PRIMARY KEY (item_id)
+    product_sales INT DEFAULT 0,
+    PRIMARY KEY (item_id),
+    FOREIGN KEY (department_id) REFERENCES department(department_id)
+);
+
+CREATE TABLE department(
+    department_id INT AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(63),
+    over_head_costs INT DEFAULT 0,
+    PRIMARY KEY (department_id)
 );
