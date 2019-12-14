35b4cc5774a776f84e1c1049acd2a3dc7bf2a44f
Will Parks <wparks08@gmail.com>
2019-12-13 18:16:04 -0800

    add departments to seeds

--- schema.sql
+++ schema.sql
@@ -3,6 +3,13 @@ DROP DATABASE IF EXISTS bamazon;
 CREATE DATABASE bamazon;
 USE bamazon;
 
+CREATE TABLE department(
+    department_id INT AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(63),
+    over_head_costs INT DEFAULT 0,
+    PRIMARY KEY (department_id)
+);
+
 CREATE TABLE product(
     item_id INT AUTO_INCREMENT NOT NULL,
     product_name VARCHAR(255),
@@ -12,11 +19,4 @@ CREATE TABLE product(
     product_sales INT DEFAULT 0,
     PRIMARY KEY (item_id),
     FOREIGN KEY (department_id) REFERENCES department(department_id)
-);
-
-CREATE TABLE department(
-    department_id INT AUTO_INCREMENT NOT NULL,
-    department_name VARCHAR(63),
-    over_head_costs INT DEFAULT 0,
-    PRIMARY KEY (department_id)
 );
--- seeds.sql
+++ seeds.sql
@@ -1,13 +1,19 @@
 USE bamazon;
 
-INSERT INTO product (product_name, department_name, price, stock_quantity)
-            VALUES  ("The Departed", "Movies", 999, 324),
-                    ("Inception", "Movies", 1499, 345),
-                    ("PS4", "Electronics", 29999, 987),
-                    ("Xbox One", "Electronics", 29999, 4567),
-                    ("Dog Food", "Pets", 2499, 367),
-                    ("Cat Food", "Pets", 2099, 482),
-                    ("Dog Collar", "Pets", 699, 641),
-                    ("Motor Oil", "Automotive", 499, 561),
-                    ("Transmission Fluid", "Automotive", 699, 194),
-                    ("Tire Pressure Gauge", "Automotive", 299, 65412);
+INSERT INTO department (department_name)
+            VALUES ("Movies"),
+                   ("Electronics"),
+                   ("Pets"),
+                   ("Automotive");
+
+INSERT INTO product (product_name, department_id, price, stock_quantity)
+            VALUES  ("The Departed", (SELECT department_id FROM department WHERE department_name="Movies"), 999, 324),
+                    ("Inception", (SELECT department_id FROM department WHERE department_name="Movies"), 1499, 345),
+                    ("PS4", (SELECT department_id FROM department WHERE department_name="Electronics"), 29999, 987),
+                    ("Xbox One", (SELECT department_id FROM department WHERE department_name="Electronics"), 29999, 4567),
+                    ("Dog Food", (SELECT department_id FROM department WHERE department_name="Pets"), 2499, 367),
+                    ("Cat Food", (SELECT department_id FROM department WHERE department_name="Pets"), 2099, 482),
+                    ("Dog Collar", (SELECT department_id FROM department WHERE department_name="Pets"), 699, 641),
+                    ("Motor Oil", (SELECT department_id FROM department WHERE department_name="Automotive"), 499, 561),
+                    ("Transmission Fluid", (SELECT department_id FROM department WHERE department_name="Automotive"), 699, 194),
+                    ("Tire Pressure Gauge", (SELECT department_id FROM department WHERE department_name="Automotive"), 299, 65412);
